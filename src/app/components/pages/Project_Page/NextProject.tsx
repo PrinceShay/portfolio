39a0d7d0be2236a46cec6f2706e2eb7cdfcf4f91
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-06-21 14:48:25 +0200

    finished Swiper

--- src/app/components/pages/Project_Page/NextProject.tsx
+++ src/app/components/pages/Project_Page/NextProject.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -7,28 +7,79 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { ProjectCard, NextProjectProps } from "@/app/lib/interface"; // Adjust the import path as necessary
+import { urlFor } from "@/app/lib/sanity";
+import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function NextProject({ projects }: NextProjectProps) {
+  const hoverRef = useRef<(HTMLAnchorElement | null)[]>([]);
+
+  useGSAP(() => {
+    hoverRef.current.forEach((hoverElem) => {
+      if (hoverElem) {
+        const NextHover = gsap.timeline({ paused: true });
+        NextHover.fromTo(
+          hoverElem.querySelector(".NextProject_Image"),
+          { scale: 1 },
+          {
+            scale: 1.2,
+            duration: 1,
+            rotateZ: 2,
+            ease: "power4.inOut",
+          }
+        );
+
+        hoverElem.addEventListener("mouseenter", () => NextHover.play());
+        hoverElem.addEventListener("mouseleave", () => NextHover.reverse());
+      }
+    });
+  }, [projects]);
+
   return (
-    <section className="px-12 md:px-24 lg:px-48">
-      <h1>Nächses Projekt</h1>
+    <section className="px-12 md:px-24 lg:px-48 py-24">
+      <h1 className="Section_Headline small mb-12">Nächses Projekt</h1>
 
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
-        slidesPerView={3}
+        slidesPerView={2}
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
       >
-        {projects.map((project: ProjectCard) => (
+        {projects.map((project: ProjectCard, index) => (
           <SwiperSlide key={project.currentSlug}>
-            <div className="min-h-48 bg-red-500">
-              <h2>{project.title}</h2>
-              {/* Add more details here as needed */}
-            </div>
+            <Link
+              ref={(el) => (hoverRef.current[index] = el)}
+              href={`/projekte/${project.currentSlug}`}
+            >
+              <div className=" h-96 relative overflow-hidden rounded-xl">
+                <img
+                  className="NextProject_Image w-full h-full absolute object-cover z-10"
+                  src={urlFor(project.titleImage).url()}
+                  alt={project.title}
+                />
+                <div className="absolute h-full w-full z-20 bg-gradient-to-t from-primary-900 to-transparent bg-opacity-60"></div>
+                <div className="relative z-30 w-full h-full flex flex-col justify-end gap-6 p-6">
+                  <h2 className="text-2xl font-bold uppercase">
+                    {project.title}
+                  </h2>
+                  <div className="flex justify-start gap-2 w-full overflow-auto">
+                    {project.categories.map((category: string) => (
+                      <div
+                        key={category}
+                        className="text-sm category py-2 px-4 relative bg-primary-300 rounded-full backdrop-blur-lg bg-opacity-10"
+                      >
+                        {category}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
