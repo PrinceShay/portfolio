f4d3f92a15f26406b1bf2d417299bd413dd914a6
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-08-26 19:02:39 +0200

    changed grain and noise

--- src/app/components/Functions/Noise.jsx
+++ src/app/components/Functions/Noise.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Noise() {
   return (
-    <div className="w-full h-screen fixed top-0 -z-10 mix-blend-screen opacity-50 pointer-events-none">
+    <div className="w-full h-screen fixed top-0 -z-10 mix-blend-screen opacity-15 pointer-events-none">
       <video muted loop autoPlay src="/assets/videos/noise.webm"></video>
     </div>
   );
