93e2412c7fb74e065c8ac877017e272ab3833e21
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-08-28 16:34:53 +0200

    fixed typo

--- src/app/components/pages/Project_Page/NextProject.tsx
+++ src/app/components/pages/Project_Page/NextProject.tsx
@@ -38,7 +38,7 @@ function NextProject({ projects }: NextProjectProps) {
 
   return (
     <section className="px-6 md:px-24 lg:px-48 py-24">
-      <h1 className="Section_Headline small mb-12">Nächses Projekt</h1>
+      <h1 className="Section_Headline small mb-12">Nächstes Projekt</h1>
 
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
