634ef0a5bbc8264a27447ff0c6bdf6b07edc034e
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-09-25 17:59:08 +0200

    fixed missing key

--- src/app/components/pages/Main_Page/Blog/BlogComponent.tsx
+++ src/app/components/pages/Main_Page/Blog/BlogComponent.tsx
@@ -35,7 +35,7 @@ digitalen Projekte!"
       <div className="grid grid-cols-12 w-full">
         <div className="col-start-1 col-end-13 grid xl:grid-cols-3 grid-flow-row gap-16 xl:gap-8 relative max-w-[1600px] mx-auto">
           {data.map((post, idx) => (
-            <Suspense fallback={<BlogItemLoader />}>
+            <Suspense key={post.id} fallback={<BlogItemLoader />}>
               <BlogItem key={post.id} post={post} idx={idx} />
             </Suspense>
           ))}
