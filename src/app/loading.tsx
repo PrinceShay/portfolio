a24c84045973e8e8136725e8cb1b82091d8555a2
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-09-26 00:18:02 +0200

    added loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,29 @@
+"use client";
+import lottie from "lottie-web";
+import { useEffect, useRef } from "react";
+
+const Loading = () => {
+  // Specify the ref type as HTMLDivElement or null to accommodate for the initial null state.
+  const animationContainer = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (animationContainer.current) {
+      const instance = lottie.loadAnimation({
+        container: animationContainer.current, // Safe to use because we checked if it's not null
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "assets/animations/loading.json",
+      });
+      return () => instance.destroy();
+    }
+  }, []);
+
+  return (
+    <div className="w-full h-screen bg-darkBlue-500 flex items-center justify-center px-6">
+      <div className="w-full max-w-64" ref={animationContainer}></div>
+    </div>
+  );
+};
+
+export default Loading;
