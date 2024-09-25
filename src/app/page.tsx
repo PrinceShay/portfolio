694fdd1513d84aa78b0f7932e6127fffca7087e9
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-09-25 18:13:35 +0200

    remove PreLoader testwise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export async function generateMetadata({
 export default function Home() {
   return (
     <main className="">
-      <Preloader />
+      {/* <Preloader /> */}
       <Hero2 />
       <Projects />
       <CustomerBenefits />
