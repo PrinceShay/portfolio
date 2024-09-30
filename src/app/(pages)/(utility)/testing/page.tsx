d1662a36f10feed0d763829b4b4fe7a011a48c6a
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-09-30 13:18:04 +0200

    addes testing page

---
+++ src/app/(pages)/(utility)/testing/page.tsx
@@ -0,0 +1,25 @@
+import BlogComponent from "@/app/components/pages/Main_Page/Blog/BlogComponent";
+import CustomerBenefits from "@/app/components/pages/Main_Page/CustomerBenefits/CustomerBenefits";
+import Hero2 from "@/app/components/pages/Main_Page/Hero/Hero2";
+import Projects from "@/app/components/pages/Main_Page/Projects/Projects";
+import Service2 from "@/app/components/pages/Main_Page/Service/Service2";
+import Preloader from "@/app/components/shared/Preloader";
+import CTAWindow from "@/app/components/shared/ui/CTAWindow";
+import React, { Suspense } from "react";
+
+export default function page() {
+  return (
+    <main className="">
+      <Preloader />
+      <Hero2 />
+      <Projects />
+      <CustomerBenefits />
+      <Service2 />
+      <CTAWindow
+        text="Schreib mir eine kurze Mail und ich melde mich bei dir."
+        title="Bereit, wenn du es bist"
+      />
+      <BlogComponent />
+    </main>
+  );
+}
