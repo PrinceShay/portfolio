2c1801ca5645b7b37c9533d968534b226a955111
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-11-13 21:33:38 +0100

    preloader

--- src/app/components/shared/Preloader.tsx
+++ src/app/components/shared/Preloader.tsx
@@ -1,41 +1,30 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
 
 function Preloader() {
   const [gridSize, setGridSize] = useState({ columns: 12, rows: 12 });
   const [bannerCount, setBannerCount] = useState(144);
   const loadingContainerRef = useRef<HTMLDivElement>(null);
-  const gridRef = useRef<HTMLDivElement>(null);
 
-  // Funktion zur Berechnung der Grid-Größe basierend auf dem Bildschirm
-  const updateGridSize = () => {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
+  useLayoutEffect(() => {
+    const updateGridSize = () => {
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+      const desiredColumns = 12; // Festgelegte Anzahl von Spalten
+      const desiredRows = 12; // Festgelegte Anzahl von Reihen
+      const cellSize = Math.min(
+        screenWidth / desiredColumns,
+        screenHeight / desiredRows
+      );
+      const columns = Math.floor(screenWidth / cellSize);
+      const rows = Math.floor(screenHeight / cellSize);
 
-    // Maximale Anzahl an Spalten und Reihen zur Begrenzung auf mobilen Geräten
-    const maxColumns = 20;
-    const maxRows = 20;
+      setGridSize({ columns, rows });
+      setBannerCount(columns * rows);
+    };
 
-    // Berechnung der Zellgröße, um quadratische Zellen zu gewährleisten
-    const cellSize = Math.floor(
-      Math.min(screenWidth / maxColumns, screenHeight / maxRows)
-    );
-
-    // Berechnung der tatsächlichen Anzahl der Spalten und Reihen, um den Bildschirm zu füllen
-    const columns = Math.ceil(screenWidth / cellSize);
-    const rows = Math.ceil(screenHeight / cellSize);
-
-    setGridSize({ columns, rows });
-    setBannerCount(columns * rows);
-  };
-
-  useEffect(() => {
-    // Initiale Berechnung der Grid-Größe
     updateGridSize();
-
-    // Aktualisierung der Grid-Größe bei Fensteränderungen
     window.addEventListener("resize", updateGridSize);
 
     return () => {
@@ -43,65 +32,44 @@ function Preloader() {
     };
   }, []);
 
-  // Verwenden des useGSAP Hooks für die Animation
-  useGSAP(
-    (context, contextSafe) => {
-      if (bannerCount > 0) {
-        const tl = gsap.timeline({
-          delay: 0.25,
-          onComplete: () => {
-            if (loadingContainerRef.current) {
-              gsap.to(loadingContainerRef.current, {
-                opacity: 0,
-                duration: 0.5,
-                onComplete: () => {
-                  if (loadingContainerRef.current) {
-                    loadingContainerRef.current.style.display = "none";
-                  }
-                },
-              });
-            }
-          },
-          context: context, // Kontext für saubere Aufräumung
-        });
+  useLayoutEffect(() => {
+    if (bannerCount > 0) {
+      const tl = gsap.timeline({
+        delay: 0.25,
+        onComplete: () => {
+          if (loadingContainerRef.current) {
+            loadingContainerRef.current.style.display = "none";
+          }
+        },
+      });
 
-        tl.to(".Banner", {
-          opacity: 0,
-          backgroundColor: "#35254D",
-          ease: "power3.out",
-          stagger: {
-            amount: 0.95,
-            from: "random",
-            grid: [gridSize.columns, gridSize.rows],
-          },
-        });
-      }
-    },
-    {
-      dependencies: [bannerCount, gridSize.columns, gridSize.rows],
-      scope: gridRef,
+      tl.to(".Banner", {
+        opacity: 0,
+        backgroundColor: "#35254D",
+        ease: "power3.out",
+        stagger: {
+          amount: 0.95,
+          from: "random",
+          grid: [gridSize.columns, gridSize.rows],
+        },
+      });
     }
-  );
+  }, [bannerCount, gridSize.columns, gridSize.rows]);
 
   return (
     <div
       ref={loadingContainerRef}
-      className="w-screen h-screen flex justify-center items-center fixed left-0 top-0 z-50 "
+      className="w-full h-screen flex justify-center items-center fixed left-0 top-0 z-[60]"
     >
       <div
-        ref={gridRef}
-        className="w-screen h-screen grid"
+        className="w-full h-screen absolute grid z-[59]"
         style={{
           gridTemplateColumns: `repeat(${gridSize.columns}, 1fr)`,
           gridTemplateRows: `repeat(${gridSize.rows}, 1fr)`,
         }}
       >
         {Array.from({ length: bannerCount }).map((_, index) => (
-          <div
-            key={index}
-            className="Banner bg-darkBlue-500 w-full h-full"
-            style={{ aspectRatio: "1 / 1" }} // Sicherstellen, dass die Zellen quadratisch bleiben
-          ></div>
+          <div key={index} className="Banner bg-darkBlue-500"></div>
         ))}
       </div>
     </div>
